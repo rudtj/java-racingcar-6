8e6654602eeda3631a7e31d0b505c5cba9b24e85
kyoungseo <98938406+rudtj@users.noreply.github.com>
2023-10-31 14:43:39 +0900

    feat: Race 클래스 구현
    자동차 경주를 관리하는 Race 클래스 구현
    -Race() : 참여하는 자동차들의 리스트와 전체 라운드 수를 입력받아 새로운 인스턴스 생성
    -increaseCoountNum : 현재까지 진행한 라운드 수를 1 증가시키는 메서드 생성

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,4 +1,27 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class Race {
+
+    private final int count;
+    private final List<Car> cars;
+    private int countNum = 0;
+
+    public Race(List<Car> cars, int count) {
+        this.cars = cars;
+        this.count = count;
+    }
+
+    public void increaseCountNum() {
+        this.countNum += 1;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
 }
